1a037ce38cc622bf041432e96428c345ee5709c1
Jacob Clark <jacobclark1198@gmail.com>
2022-05-12 07:39:22 +0000

    'Methods' exercises

---
+++ methods/ex4.rb
@@ -0,0 +1,10 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+#the above code will not print anything because it never reaches
+#line 4 because of the return on line 3
---
+++ methods/ex5.rb
@@ -0,0 +1,8 @@
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+#the scream method returns nil
